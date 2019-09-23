55614f5e9d76427386b75a760f137c3885381534
Ben Franzoso <benfranzoso38@gmail.com>
2019-09-23 00:00:50 -0400

    contentful working

---
+++ src/components/Home/Menu.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import Img from "gatsby-image"
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props)
+    console.log(props.items)
+  }
+  render() {
+    return <div>hello</div>
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -19,6 +20,7 @@ const IndexPage = ({ data }) => (
       ctalink="/about/"
       ctatext="About Page"
     />
+    <Menu items={data.menu} />
   </Layout>
 )
 
@@ -28,6 +30,24 @@ export const query = graphql`
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
