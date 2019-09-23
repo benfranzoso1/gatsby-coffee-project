8c74b13ffe5c5f183f8190425fd31c98cab77173
Ben Franzoso <benfranzoso38@gmail.com>
2019-09-22 20:01:51 -0400

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About Page </h1>
+    <p>this is the about page</p>
+  </Layout>
+)
+
+export default AboutPage
